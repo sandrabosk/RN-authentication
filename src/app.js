b23e8ec06c0aea1c1537d0a13a56d4258b3107ec
sandrabosk <sandraboskovic@hotmail.com>
2017-12-01 02:18:22 -0500

    working on Card and CardSection

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+// import firebase needs to go before other imports
+import firebase from 'firebase';
+import { View } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBl5n6NbQT8hFxqnpEX0Gefcoc_al8T-Os',
+      authDomain: 'rn-auth-8c459.firebaseapp.com',
+      databaseURL: 'https://rn-auth-8c459.firebaseio.com',
+      projectId: 'rn-auth-8c459',
+      storageBucket: 'rn-auth-8c459.appspot.com',
+      messagingSenderId: '573448177503'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication"/>
-        <Text>
-          An App
-        </Text>
+        <LoginForm />
       </View>
     );
   }
