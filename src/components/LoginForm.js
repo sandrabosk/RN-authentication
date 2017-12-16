1815bb9b20633b91181e0dba179b4a37ab5be52a
sandrabosk <sandraboskovic@hotmail.com>
2017-12-16 15:44:26 -0500

    changes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -10,7 +10,7 @@ class LoginForm extends Component {
         <CardSection>
           <TextInput
             value={this.state.text}
-            onChangeText = { text => this.setSession({ text: text }) }
+            onChangeText = { text => this.setState({ text: text }) }
 
             style={{ height: 20, width: 100 }} />
         </CardSection>
