032dd7eda5d88260ae0cf53a7da3e6fdf7fc7b0a
sandrabosk <sandraboskovic@hotmail.com>
2017-12-18 19:17:36 -0500

    input component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,6 +4,9 @@ import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   state = { text: '' };
+  // we are not letting text exist inside the text input
+  // setting the state like this we always know what is the value of text
+  // by typing just this.setState.text
   render() {
     return(
       <Card>
@@ -11,8 +14,8 @@ class LoginForm extends Component {
           <TextInput
             value={this.state.text}
             onChangeText = { text => this.setState({ text: text }) }
-
-            style={{ height: 20, width: 100 }} />
+            style={{ height: 20, width: 100 }}
+          />
         </CardSection>
 
         <CardSection />
