73d70d02d7e4cd47349695340f5628c36bdb9f08
sandrabosk <sandraboskovic@hotmail.com>
2017-12-20 22:36:52 -0500

    styles

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '' };
+  // we are changing this to email so it's pretty clear
+  // that we are talking about email since we will have more than one text inputs
+
+  // state = { text: '' };
   // we are not letting text exist inside the text input
   // setting the state like this we always know what is the value of text
   // by typing just this.setState.text
@@ -12,9 +16,14 @@ class LoginForm extends Component {
       <Card>
         <CardSection>
           <Input
+            placeholder="user@gmail.com"
             label = "Email"
-            value={this.state.text}
-            onChangeText = { text => this.setState({ text: text }) }
+            // value={this.state.text}
+            value={this.state.email}
+
+            // onChangeText = { text => this.setState({ text: text }) }
+            onChangeText = { email => this.setState({ email }) }
+
             // style={{ height: 20, width: 100 }} => since we defined style in the Input component already, we don't need it here
           />
         </CardSection>
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -3,7 +3,7 @@ import { TextInput, View, Text } from 'react-native';
 
 // here we are receiving value and onChangeText props from LoginForm component
 // and we are passing it down to <TextInput>
-const Input = ({ label, value, onChangeText}) => {
+const Input = ({ label, value, onChangeText, placeholder}) => {
   const{ inputStyle, labelStyle, containerStyle } = styles;
 
 
@@ -11,15 +11,18 @@ const Input = ({ label, value, onChangeText}) => {
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        placeholder = {placeholder}
+        autoCorrect={false}
         style={inputStyle}
         value={value}
         onChangeText={onChangeText}
-        style={{height: 20, width: 100}}
+        // style={{height: 20, width: 100}}
       />
     </View>
   );
 };
 
+// adding styles:
 const styles = {
   inputStyle:{
     color:'#000',
@@ -42,4 +45,9 @@ const styles = {
   }
 };
 
+// explanation for the FLEX:
+// both, the input and the text, are the children of the View tag, or better to say- they are SIBILINGS
+// whenever we have sibilings and if we define flex property, that is the way how we alocate or proportion available space for them
+// the rule is: for each sibiling we add up the value of the flex (in this case 2+1=3), and that means that 2/3 of the available space
+// will belong to the input and 1/3 will be dedicated to the label
 export { Input };
